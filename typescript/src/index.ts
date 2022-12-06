4b779b219d7da0911be3dc7aaf6119300fffacac
Ahmed Soltan <75265944+sultan308@users.noreply.github.com>
2022-12-06 22:34:02 +0000

    entry point

---
+++ typescript/src/index.ts
@@ -0,0 +1,11 @@
+import { dayOnePartOne, dayOnePartTwo } from "./day1/day1";
+import { dayTwoPartOne } from "./day2/day2";
+import { dayFivePartOne } from "./day5/day5";
+import { daySixPartOne, daySixPartTwo } from "./day6/day6";
+
+//dayOnePartOne();
+//dayOnePartTwo();
+dayTwoPartOne();
+//dayFivePartOne();
+daySixPartOne();
+daySixPartTwo();
